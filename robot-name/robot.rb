3fb4029fb5f083dba26eca9c9c78fbb444f9fbf4
Charles Painter <cpainter85@gmail.com>
2015-02-17 10:54:33 -0700

    Robot is given name and name can be reset

--- robot-name/robot.rb
+++ robot-name/robot.rb
@@ -1,17 +1,17 @@
 class Robot
-  def initialize
-    @name = ((0...2).map{ [*'A'..'Z'].sample }+(0...3).map{ [*'0'..'9'].sample }).join
-  end
+  attr_accessor :name
 
-  def name
-    @name
+  def initialize
+    @name = generate_random_name
   end
 
   def reset
-    @name = ((0...2).map{ [*'A'..'Z'].sample }+(0...3).map{ [*'0'..'9'].sample }).join
+    @name = generate_random_name
+  end
+
+  private
+
+  def generate_random_name
+    ((0...2).map{ [*'A'..'Z'].sample }+(0...3).map{ [*'0'..'9'].sample }).join
   end
 end
-
-
-# range = [*'0'..'9',*'A'..'Z',*'a'..'z']
-#     (0...n).map{ range.sample }.join
